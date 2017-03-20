fef77b2e2ceae80a219081326434e82e93de5d40
黎兆旺 <siuwongli1994@gmail.com>
2017-03-20 22:04:45 +0800

    采用Generator+co,去掉mongoose，采用mongodb driver

--- app/models/user.js
+++ app/models/user.js
@@ -1,16 +1,53 @@
 /**
  * Created by SiuWongLi on 17/3/11.
  */
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
-var UserSchema = new Schema({
-    username:{type:String,unique:true},
-    password:String,
-    type:Number,
-    major:String,
-    className:String,
-    sex:String,
-    name:String,
-    studentId:String
-});
-module.exports= mongoose.model('user',UserSchema);
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+// Connection URL
+var url = 'mongodb://localhost:27017/ets';
+var co = require('co');
+// Use connect method to connect to the server
+var UserManage = {
+    find: function (query,cb) {
+        co(function *() {
+            var db = yield MongoClient.connect(url);
+            var collection = db.collection('users');
+            var result = yield collection.findOne(query, {"_id": 1, "username": 1});
+            yield db.close();
+            if(cb&&typeof cb=='function'){
+                cb(result);
+            }
+        })
+    }, update: function (query, data, cb) {
+        co(function *() {
+            var db = yield MongoClient.connect(url);
+            var collection = yield db.collection('users');
+            var result = yield collection.updateOne(query, data, {upsert: true});
+            yield db.close();
+            if(cb&&typeof cb=='function'){
+                cb(result);
+            }
+        })
+    }, delete: function () {
+
+    }, add: function (user, cb) {
+        co(function *() {
+            var db = yield MongoClient.connect(url);
+            var collection = yield db.collection('users');
+            var result = yield collection.insertOne(user);
+            yield db.close();
+            if(cb&&typeof cb=='function'){
+                cb(result);
+            }
+        })
+    }, login: function (uid, pw,cb) {
+        co(function *() {
+            var collection = db.collection('users');
+            var result = yield collection.findOne({username: uid, password: pw});
+            if(cb&&typeof cb=='function'){
+                cb(result);
+            }
+        })
+    }
+}
+module.exports = UserManage;
---
+++ app/routers/user.js
@@ -0,0 +1,49 @@
+/**
+ * Created by SiuWongLi on 17/3/17.
+ */
+// more routes for our users will happen here
+
+var express = require('express'), assert = require('assert');
+var router = express.Router();
+var userManage = require("./../models/user");
+var co = require('co');
+//router.post('/infos', function (req, res) {
+router.post('/infos', function (req, res) {
+//    var {uid,pw} = req.body;
+    var query = {};
+    userManage.find(query, function (result) {
+        res.json(result);
+    });
+})
+router.post('/update', function (req, res) {
+
+})
+router.post('/register', function (req, res) {
+    var {username,password,type,email} = req.query;
+    var person = {username:username,password:password,type:type,email:email};
+    userManage.add(person, function (err) {
+        if (err) {
+            if (err.code == 11000) {
+                res.json({result: '该用户已被注册！'})
+            } else {
+                res.json(err);
+            }
+        } else {
+            res.json({result: 'ok'});
+        }
+    });
+})
+router.post('/login', function (req, res) {
+    var {uid,pw} = req.bdoy;
+    if (uid && pw) {
+        var query = {username: uid, password: pw};
+        userManage.find(query, function (result) {
+            if (result) { // 登录成功
+                res.json({login: true})
+            } else {
+                res.json({login: false, err: '用户名或密码错误'});
+            }
+        });
+    }
+})
+module.exports = router;
