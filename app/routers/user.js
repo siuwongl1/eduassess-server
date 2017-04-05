b1a77661a73ac49151b01b2c648b93b55caf71d7
黎兆旺 <siuwongli1994@gmail.com>
2017-04-05 13:41:51 +0800

    添加修改密码功能

--- app/routers/user.js
+++ app/routers/user.js
@@ -15,6 +15,7 @@ var multipartMiddleware = multipart();
 //router.post('/infos', function (req, res) {
 router.get('', multipartMiddleware,function (req, res) {
     var resp = new ResponseEntity();
+    //查询相关的用户信息，参数为空时，默认查询全部
     co(function*() {
         var {uid,type,name,cls,pro} = req.query;
         //default to get all users
@@ -52,19 +53,19 @@ router.get('', multipartMiddleware,function (req, res) {
     });
 });
 router.put('',multipartMiddleware, function (req, res) {
-    //update user infomation
+    //修改个人信息
     var resp = new ResponseEntity();
     co(function *() {
         var {uid,type} = req.body;
         if (ObjectID.isValid(uid) && type) {
-            if (type == 'student') {
+            if (type == 1) {
                 //学生信息修改
                 var {uid,cls,name,sex,stuid,pro} = req.body;
                 var user = {uid: uid, cls: cls, name: name, sex: sex, stuid: stuid, pro};
                 userManage.update({_id: new ObjectID(id)}, user);
-            } else if (type == 'teacher') {
+            } else if (type == 2) {
                 //教师信息修改
-            } else if (type == 'instruct') {
+            } else if (type == 3) {
                 //教学管理者修改
             }
             resp.setStatusCode(0);
@@ -80,7 +81,7 @@ router.put('',multipartMiddleware, function (req, res) {
     });
 });
 router.post('', multipartMiddleware,function (req, res) {
-    //register user
+    //用户注册
     var resp = new ResponseEntity();
     co(function *() {
         var {uid,pw,type,email} = req.body;
@@ -112,6 +113,7 @@ router.post('/login',multipartMiddleware, function (req, res) {
             var query = {username: uid, password: pw, type: type};
             var result = yield userManage.login(query);
             if (result) { // 登录成功
+                resp.setData(result._id);
                 resp.setStatusCode(0);
                 resp.setMessage('登录成功');
             } else {
@@ -120,6 +122,33 @@ router.post('/login',multipartMiddleware, function (req, res) {
             }
         } else {
             resp.setMessage("用户名,密码,用户类型不能为空");
+            resp.setStatusCode(1);
+        }
+        res.json(resp);
+    }).catch((err)=>{
+        resp.setMessage(err);
+        resp.setStatusCode(1);
+        res.json(resp);
+    });
+})
+router.put('/mpw',multipartMiddleware, function (req, res) {
+    //修改密码
+    var resp = new ResponseEntity();
+    co(function *() {
+        var {uid,pw,origin} = req.body;
+        if (uid && pw && origin) {
+            var query = {username: uid, password: origin};
+            var data ={password:pw}
+            var result = yield userManage.update(query,data);
+            if (result) { //修改成功
+                resp.setStatusCode(0);
+                resp.setMessage('修改成功');
+            } else {
+                resp.setMessage('用户名或密码错误');
+                resp.setStatusCode(1);
+            }
+        } else {
+            resp.setMessage("用户名,新密码,原始密码不能为空");
             resp.setStatusCode(1);
         }
         res.json(resp);
